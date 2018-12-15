1e44124307ec4d3167b67cc48c1765fcdcb3e91b
Raj Shekhar <rajshekhar7@hotmail.com>
2018-12-16 00:36:41 +0530

    Add project files.

---
+++ SFML Project/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(512, 512),"SFML Tutorial" , sf::Style::Close | sf::Style::Resize);
+
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+			{
+				case 
+			}
+		}
+	}
+
+
+	return 0;
+}
