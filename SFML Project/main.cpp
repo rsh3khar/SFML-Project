bb27a2e3e5114412b5e2577518fdc23ba51cdc9e
Raj Shekhar <rajshekhar7@hotmail.com>
2018-12-17 20:01:03 +0530

    keyboard and mouse input

--- SFML Project/main.cpp
+++ SFML Project/main.cpp
@@ -4,6 +4,9 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(512, 512),"SFML Tutorial" , sf::Style::Close | sf::Style::Resize);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	player.setFillColor(sf::Color::Magenta);
+	player.setOrigin(50.0f, 50.0f);
 
 	while (window.isOpen())
 	{
@@ -25,8 +28,34 @@ int main()
 				}
 			}
 		}
-	}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			player.move(0.0f, -0.5f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			player.move(-0.5f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			player.move(0.0f, 0.5f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			player.move(0.5f, 0.0f);
+		}
+
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+			player.setPosition((float)mousePos.x, (float)mousePos.y);
+		}
+
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
 
 	return 0;
 }
