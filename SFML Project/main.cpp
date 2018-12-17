fe602e6550322c42cb44b3c027b5da6dacc2b56c
Raj Shekhar <rajshekhar7@hotmail.com>
2018-12-18 02:58:11 +0530

    use one texture from multiple

--- SFML Project/main.cpp
+++ SFML Project/main.cpp
@@ -4,11 +4,17 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(512, 512),"SFML Tutorial" , sf::Style::Close | sf::Style::Resize);
-	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	sf::RectangleShape player(sf::Vector2f(100.0f, 150.0f));
 	player.setPosition(206.0f, 206.0f);
 	sf::Texture playerTexture;
-	playerTexture.loadFromFile("../Texture/tux_from_linux.png");
+	playerTexture.loadFromFile("tux_from_linux.png");
 	player.setTexture(&playerTexture);
+
+	sf::Vector2u textureSize = playerTexture.getSize();
+	textureSize.x /= 3;
+	textureSize.y /= 9;
+
+	player.setTextureRect(sf::IntRect(textureSize.x * 2, textureSize.y * 8, textureSize.x, textureSize.y));
 
 	while (window.isOpen())
 	{
